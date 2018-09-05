582f19abec4b0df589cdab827f6f137239dd33c5
bl3z3 <patrick.blaise@students.jkuat.ac.ke>
2018-09-06 01:20:13 +0300

    Added cron jobs to print hello and also to send emails automatically without the app running

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_crontab',
     'cron',
 ]
 
@@ -125,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRONJOBS = [
+    ('*/20 * * * *', 'django.core.management.email_reminder'),
+]
